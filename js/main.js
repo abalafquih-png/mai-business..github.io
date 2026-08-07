0388ab81bb0840cba190152e84e66a10e49304dd
abalafquih-png <a.balafquih@gmail.com>
2026-08-08 01:39:00 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-/* Lógica principal de interactividad para MAI Business Lab */
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("MAI Business Lab - Sistema cargado correctamente.");
-
-    // Puedes añadir aquí futuras funciones adicionales, como validación de formularios,
-    // efectos de desplazamiento suave (smooth scroll) o alertas dinámicas.
-});
